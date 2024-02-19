fa0c0c30080db30c24b90081345b8c55552636cb
Valentina Grinchik <valushagrinchik@gmail.com>
2024-02-19 14:27:28 +0300

    fix seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-import { groupBy } from 'lodash';
 const prisma = new PrismaClient();
 
 const seedUsers = async () => {
@@ -26,97 +25,8 @@ const seedUsers = async () => {
   console.log(admin, worker);
 };
 
-const seedStubData = async () => {
-  const groupPromises = [
-    {
-      id: 1,
-      name: 'TestGroup1',
-    },
-    {
-      id: 2,
-      name: 'TestGroup2',
-    },
-    {
-      id: 3,
-      name: 'TestGroup3',
-    },
-    {
-      id: 4,
-      name: 'WWW Group',
-    },
-    {
-      id: 5,
-      name: 'DDD Group',
-    },
-    {
-      id: 6,
-      name: 'TestGroup4',
-    },
-    {
-      id: 7,
-      name: 'AAA Group',
-    },
-    {
-      id: 8,
-      name: '1234 Group',
-    },
-  ].map((group) =>
-    prisma.group.upsert({
-      where: {
-        id: group.id,
-      },
-      update: group,
-      create: group,
-    }),
-  );
-  const categoryPromises = [
-    {
-      id: 1,
-      name: 'CAt 2',
-      groupId: 3,
-    },
-    {
-      id: 2,
-      name: 'CAt 3',
-      groupId: 3,
-    },
-    {
-      id: 3,
-      name: 'AA CAt 2',
-      groupId: 3,
-    },
-    {
-      id: 4,
-      name: 'CAtw 2',
-      groupId: 1,
-    },
-    {
-      id: 5,
-      name: '234 CAt 33',
-      groupId: 1,
-    },
-    {
-      id: 6,
-      name: 'AA CAt 22',
-      groupId: 1,
-    },
-  ].map((category) =>
-    prisma.category.upsert({
-      where: {
-        id: category.id,
-      },
-      update: category,
-      create: category,
-    }),
-  );
-
-  await Promise.all(groupPromises);
-  await Promise.all(categoryPromises);
-};
-
 async function main() {
   await seedUsers();
-  await seedStubData();
 }
 main()
   .then(async () => {
