d8a91a4e43c1beb5183e9bf0d53036ec6c768710
Valentina Grinchik <valushagrinchik@gmail.com>
2024-02-19 11:42:00 +0300

    fix start prod command and enable logs

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { PrismaExceptionFilter } from './prisma-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new PrismaExceptionFilter());
   await app.listen(3001);
